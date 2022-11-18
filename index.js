aa1502895b555f606f57bc0d3dfae501c029351f
stevencueto <steven.cueto1305@gmail.com>
2022-11-18 12:17:08 -0500

    refactor: join chat, added the find or create function to return the chat to the users

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const io = Server(PORT, {
         // credentials: true
       }
 });
-
+const Chat = require('./model/Chat');
 
 const mongoose = require('mongoose')
 const mongoURI = process.env.MONGO_URI
@@ -24,17 +24,31 @@ mongoose.connect(mongoURI, { useNewUrlParser: true},
 const db = mongoose.connection
 db.on('error', err => console.log(err.message + ' is Mongod not running?'))
 db.on('disconnected', () => console.log('mongo disconnected', mongoURI))
-io.on('connection', socket => {
-  const id = socket.handshake.query.id
-  socket.join(id)
 
-  socket.on('send-message', ({ recipients, text }) => {
-    recipients.forEach(recipient => {
-      const newRecipients = recipients.filter(r => r !== recipient)
-      newRecipients.push(id)
-      socket.broadcast.to(recipient).emit('receive-message', {
-        recipients: newRecipients, sender: id, text
-      })
-    })
+
+io.on('connection', (socket)=>{
+  socket.on('find-chat', async (users)=>{
+      try{
+          let chat = await Chat.find({users: { $and:  [users[0] ,users[1] ] }})
+          if (!chat){
+            chat = await Chat.create({users: users})
+          }
+          socket.join(chat._id)
+          socket.emit('load-chat', chat)
+          
+          // socket.on('send-changes', (delta)=>{
+          //     socket.broadcast.to(id).emit('receive-changes', delta)
+          // })
+          
+          // socket.on('save-document', async(data)=>{
+          //     try{
+          //         await DocData.findByIdAndUpdate(mongoose.Types.ObjectId(id), {data}, {new:true})
+          //     }catch(err){
+          //         console.log(err.message, "or here")
+          //     } 
+          // })   
+      }catch(err){
+          console.log(err.message, 'here?')
+      }
   })
 })
